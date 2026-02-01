cb4bed27c644722acbf8c391805a2fca6313ba46
Alexis Bon <alexisbon73@protonmail.com>
2026-02-01 22:29:20 +0100

    Refactor: state

--- src/game/core/mod.rs
+++ src/game/core/mod.rs
@@ -1,20 +1,10 @@
 use crate::game::{game_action::GameAction, state::State};
 
-impl State {
-    pub fn increment_level(&mut self) {
-        self.set_level(self.get_level() + 1);
-    }
-
-    pub fn decrement_level(&mut self) {
-        let l = self.get_level();
-        self.set_level(if l > 0 {l - 1} else {l});
-    }
-}
-
 pub fn perform_action(state: &mut State, action: GameAction) {
     match action {
         GameAction::Left => state.decrement_level(),
         GameAction::Right => state.increment_level(),
+        GameAction::Rotate => state.increment_rotation(),
         _ => ()
     }
 }
--- src/game/state.rs
+++ src/game/state.rs
@@ -2,8 +2,15 @@ use crate::game;
 use game::cell::Cell;
 use game::tetromino::Tetromino;
 
+pub struct CurrentTetromino {
+    tetromino: Tetromino,
+    position: (usize, usize),
+    rotation: usize
+}
+
 pub struct State {
     grid: [Cell; game::GRID_LENGTH],
+    current_tetromino: CurrentTetromino,
     hold: Option<Tetromino>,
     next_tetrominos_queue: [Option<Tetromino>; game::NEXT_TETROMINOS_QUEUE_SIZE],
     score: i32,
@@ -15,6 +22,11 @@ impl State {
     pub fn new() -> State {
         State {
             grid: [Cell::Empty; game::GRID_LENGTH],
+            current_tetromino: CurrentTetromino {
+                tetromino: Tetromino::J,
+                position: (1, 4),
+                rotation: 0
+            },
             hold: None,
             next_tetrominos_queue: [None; game::NEXT_TETROMINOS_QUEUE_SIZE],
             score: 0,
@@ -23,11 +35,34 @@ impl State {
         }
     }
 
+    pub fn get_current_tetromino(&self) -> Tetromino {
+        self.current_tetromino.tetromino
+    }
+
+    pub fn get_current_tetromino_position(&self) -> (usize, usize) {
+        self.current_tetromino.position
+    }
+
+    pub fn get_current_tetromino_rotation(&self) -> usize {
+        self.current_tetromino.rotation
+    }
+
     pub fn get_level(&self) -> u32 {
         self.level
     }
+}
 
-    pub fn set_level(&mut self, level: u32) {
-        self.level = level;
+impl State {
+    pub fn increment_level(&mut self) {
+        self.level += 1;
+    }
+
+    pub fn decrement_level(&mut self) {
+        let l = self.level;
+        self.level = if l > 0 {l - 1} else {l};
+    }
+
+    pub fn increment_rotation(&mut self) {
+        self.current_tetromino.rotation = (self.current_tetromino.rotation + 1) % 4;
     }
 }
