565ed83958d3377de66cc29c6215be9a25d463de
Alexis Bon <alexisbon73@protonmail.com>
2026-02-01 22:30:03 +0100

    Fix: input

--- src/game/view/input.rs
+++ src/game/view/input.rs
@@ -1,22 +1,28 @@
 use std::time::Duration;
-use crossterm::event::{self, Event, KeyCode};
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 
 use crate::game::game_action::GameAction;
 
 pub fn read() -> Option<GameAction> {
-    if let Ok(_) = event::poll(Duration::from_millis(40)) {
-        if let Ok(Event::Key(key_event)) = event::read() {
-            return match key_event.code {
-                KeyCode::Left => Some(GameAction::Left),
-                KeyCode::Right => Some(GameAction::Right),
-                KeyCode::Down => Some(GameAction::Down),
-                KeyCode::Char('r') => Some(GameAction::Rotate),
-                KeyCode::Char('s') => Some(GameAction::Store),
-                KeyCode::Char('p') => Some(GameAction::Pause),
-                KeyCode::Esc => Some(GameAction::Quit),
-                _ => None
+    if let Ok(is_event) = event::poll(Duration::from_millis(0)) {
+        if is_event {
+            if let Ok(Event::Key(key_event)) = event::read() {
+                if key_event.kind == KeyEventKind::Press {
+                    
+                    return match key_event.code {
+                        KeyCode::Left => Some(GameAction::Left),
+                        KeyCode::Right => Some(GameAction::Right),
+                        KeyCode::Down => Some(GameAction::Down),
+                        KeyCode::Char('r') => Some(GameAction::Rotate),
+                        KeyCode::Char('s') => Some(GameAction::Store),
+                        KeyCode::Char('p') => Some(GameAction::Pause),
+                        KeyCode::Esc => Some(GameAction::Quit),
+                        _ => None
+                    }
+                }
             }
         }
+
     }
 
     None
